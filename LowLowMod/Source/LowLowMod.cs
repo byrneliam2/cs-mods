50e620fca757df88a238bb9b7aa441d04fec21a5
Liam <liam.jb1@gmail.com>
2018-01-29 22:33:49 +1300

    Compress properties

--- LowLowMod/Source/LowLowMod.cs
+++ LowLowMod/Source/LowLowMod.cs
@@ -5,20 +5,8 @@ namespace LLMod
 {
     public class LowLowModBase : IUserMod
     {
-        public string Name
-        {
-            get
-            {
-                return "LowLowMod: Height and Level Restrictor";
-            }
-        }
+        public string Name => "LowLowMod: Height and Level Restrictor";
 
-        public string Description
-        {
-            get
-            {
-                return "Restricts the height of low commercial buildings by limiting their maximum level.";
-            }
-        }
+        public string Description => "Restricts the height of low commercial buildings by limiting their maximum level.";
     }
 }
