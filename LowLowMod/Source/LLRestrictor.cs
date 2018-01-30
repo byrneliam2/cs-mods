7df73ce722d5dff22bca4fd496e921fb5c9efbac
Liam <liam.jb1@gmail.com>
2018-01-30 20:24:04 +1300

    Implementation experiments

---
+++ LowLowMod/Source/LLRestrictor.cs
@@ -0,0 +1,19 @@
+﻿using ICities;
+
+namespace LowLowMod
+{
+    class LLRestrictor : LevelUpExtensionBase
+    {
+        public override void OnCreated(ILevelUp levelUp)
+        {
+            base.OnCreated(levelUp);
+        }
+
+        public override CommercialLevelUp OnCalculateCommercialLevelUp(CommercialLevelUp levelUp, int averageWealth, int landValue, ushort buildingID, Service service, SubService subService, Level currentLevel)
+        {
+            levelUp.targetLevel = Level.Level1;
+            return base.OnCalculateCommercialLevelUp(levelUp, averageWealth, landValue, buildingID, service, subService, currentLevel);
+        }
+
+    }
+}
