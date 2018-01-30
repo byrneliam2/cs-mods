31715144b86bc37622a9b179166ed757741d5071
Liam <liam.jb1@gmail.com>
2018-01-30 19:02:57 +1300

    Renaming

---
+++ LowLowMod/Source/LLModBase.cs
@@ -0,0 +1,23 @@
+﻿using ICities;
+
+namespace LowLowMod
+{
+    public class LLModBase : IUserMod
+    {
+        public string Name
+        {
+            get
+            {
+                return "LowLowMod: Height and Level Restrictor";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Restricts the height of low commercial buildings by limiting their maximum level.";
+            }
+        }
+    }
+}
